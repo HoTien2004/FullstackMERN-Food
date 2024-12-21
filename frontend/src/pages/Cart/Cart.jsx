d926ed443faa69eccc4a26e4025f20d877ddd4c0
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-12-21 22:44:01 +0700

    4. Create Place order page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
+
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+                </div>
+                <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Tổng</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Tổng phụ</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Phí vận chuyển</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Tổng tiền</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button onClick={() => navigate('./order')}>TIẾN HÀNH THANH TOÁN</button>
+        </div>
+        <div className="cart-promo-code">
+          <div>
+            <p>Nếu bạn có mã khuyến mãi, Nhập tại đây</p>
+            <div className='cart-promo-code-input'>
+              <input type="text" placeholder='mã khuyến mãi' />
+              <button>Xác nhận</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
-    return (
-        <div>
 
-        </div>
+    const { getTotalCartAmount } = useContext(StoreContext);
+
+    return (
+        <form className='place-order'>
+            <div className="place-order-left">
+                <p className="title">Thông tin vận chuyển</p>
+                <div className="multi-fields">
+                    <input type="text" placeholder='Tên' />
+                    <input type="text" placeholder='Họ' />
+                </div>
+                <input type="email" placeholder='Email' />
+                <input type="text" placeholder='Địa chỉ' />
+                <div className="multi-fields">
+                    <input type="text" placeholder='Thành phố' />
+                    <input type="text" placeholder='Quận' />
+                </div>
+                <div className="multi-fields">
+                    <input type="text" placeholder='Mã vùng' />
+                    <input type="text" placeholder='Quốc gia' />
+                </div>
+                <input type="text" placeholder='Số điện thoại' />
+            </div>
+            <div className="place-order-right">
+                <div className="cart-total">
+                    <h2>Tổng</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Tổng phụ</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Phí vận chuyển</p>
+                            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <b>Tổng tiền</b>
+                            <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+                        </div>
+                    </div>
+                    <button>THANH TOÁN</button>
+                </div>
+            </div>
+        </form>
     )
 }
 
