77f181fd7cb711615c5db7756b1cbe9f9f89fb3c
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-01-05 20:59:14 +0700

    11. Create user authentication (login & registration) feature 2

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,25 +1,59 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+import axios from "axios"
 
 const LoginPopup = ({ setShowLogin }) => {
 
+    const { url, setToken } = useContext(StoreContext)
+
     const [currState, setCurrState] = useState("Đăng nhập");
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({ ...data, [name]: value }))
+    }
+
+    const onLogin = async (event) => {
+        event.preventDefault();
+        let newUrl = url;
+        if (currState === "Đăng nhập") {
+            newUrl += "/api/user/login"
+        } else {
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl, data);
+
+        if (response.data.success) {
+            setToken(response.data.token);
+            localStorage.setItem("token", response.data.token);
+            setShowLogin(false);
+        } else {
+            alert(response.data.message);
+        }
+    }
 
     return (
         <div className='login-popup'>
-            <form className="login-popup-container">
+            <form onSubmit={onLogin} className="login-popup-container">
                 <div className="login-popup-title">
                     <h2>{currState}</h2>
                     <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
                 </div>
                 <div className="login-popup-inputs">
-                    {currState === "Đăng nhập" ? <></> : <input type="text" placeholder='Nhập tên của bạn' required />}
-
-                    <input type="email" placeholder='Nhập email của bạn' required />
-                    <input type="password" placeholder='Nhập mật khẩu của bạn' required />
+                    {currState === "Đăng nhập" ? <></> : <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Nhập tên của bạn' required />}
+                    <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Nhập email của bạn' required />
+                    <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Nhập mật khẩu của bạn' required />
                 </div>
-                <button>{currState === "Đăng ký" ? "Tạo tài khoản" : "Đăng nhập"}</button>
+                <button type='submit'>{currState === "Đăng ký" ? "Tạo tài khoản" : "Đăng nhập"}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" required />
                     <p>Tôi đồng ý với Chính sách bảo mật</p>
