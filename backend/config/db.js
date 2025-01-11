3335374bd1d79b8230bc3ca02948c40a4d70f696
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-01-11 15:55:48 +0700

    18. Add .env.example

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://tienho:18102004@cluster0.novwi.mongodb.net/food-del').then(() => console.log("DB Connected"))
+    await mongoose.connect(process.env.DB).then(() => console.log("DB Connected"))
 }
